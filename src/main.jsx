3ccfaa74d2ba82af43feebcf061aa46d74ad231d
tabrisyuen-svg <tabrisyuen@gmail.com>
2026-08-31 17:28:12 +0800

    Replace Frontend component with App component

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Frontend from './Frontend'
+import App from './App'          //
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Frontend />
+    <App />
   </React.StrictMode>
 )
