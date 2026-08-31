a4b7c8b5968a5ac0cd38247b27be306b3040609a
tabrisyuen-svg <tabrisyuen@gmail.com>
2026-08-31 17:27:24 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Admin from './Admin'
+import Frontend from './Frontend'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Admin />
+    <Frontend />
   </React.StrictMode>
 )
